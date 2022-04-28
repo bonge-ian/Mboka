b8c7d043484350b710b909734601a7c40cd7027c
Bonge <nkaranjah@gmail.com>
2022-04-28 18:37:39 +0300

    Update session driver and added session blade component

---
+++ resources/views/components/session-alert.blade.php
@@ -0,0 +1,9 @@
+@if (session('error'))
+    <x-alert type="danger"
+             :message="session('error')" />
+@endif
+
+@if (session('success'))
+    <x-alert type="success"
+             :message="session('success')" />
+@endif
