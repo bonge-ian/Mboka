3458be82b87ef5d4afacb4c5003e582984c9743f
Bonge <nkaranjah@gmail.com>
2022-04-28 18:40:16 +0300

    Update status-alert.blade.php

--- resources/views/components/status-alert.blade.php
+++ resources/views/components/status-alert.blade.php
@@ -8,8 +8,8 @@
 		'recovery-codes-generated' => __('Two Factor recovery codes have been regenerated'),
 		'two-factor-authentication-enabled' => __('Two factor authentication has been enabled'),
 		'two-factor-authentication-disabled' => __('Two factor authentication has been disabled'),
-		'verification-link-sent' => __('A new verification link has been sent to the email address you provided during
-		registration.')
+		'verification-link-sent' => __('A new verification link has been sent to the email address you provided during registration.'),
+        'create-listing' => __('Successfully, created your listing'),
 		];
 
 		$message = array_key_exists($status, $statuses) ? $statuses[$status]: $status;
