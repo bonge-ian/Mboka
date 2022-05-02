10b29179457193f1062e8fc6f9f459c598f57afc
Bonge <nkaranjah@gmail.com>
2022-05-02 12:18:47 +0300

    Disable fortify's registration functionality

--- config/fortify.php
+++ config/fortify.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Providers\RouteServiceProvider;
 use Laravel\Fortify\Features;
+use App\Providers\RouteServiceProvider;
 
 return [
 
@@ -132,7 +132,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
