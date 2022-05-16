9ee8bb730ce301d02890bd535a7ffe968abb3b62
Bonge <nkaranjah@gmail.com>
2022-05-16 17:58:48 +0300

    Update dashboard route name

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,14 @@
 <header class="uk-sticky uk-position-z-index-large"
-    uk-sticky="sel-target: .uk-background-default; cls-active: uk-navbar-sticky;animation: uk-animation-slide-top;show-on-up: true">
+        uk-sticky="sel-target: .uk-background-default; cls-active: uk-navbar-sticky;animation: uk-animation-slide-top;show-on-up: true">
     <div class="uk-background-default uk-box-shadow-small">
         <div class="uk-container uk-container-large">
-            <nav class="uk-navbar" role="main-navigation" uk-navbar="align: left">
+            <nav class="uk-navbar"
+                 role="main-navigation"
+                 uk-navbar="align: left">
                 <div class="uk-navbar-left">
-                    <a class="uk-navbar-item uk-logo" href="/" uk-icon="icon: logo; ">
+                    <a class="uk-navbar-item uk-logo"
+                       href="/"
+                       uk-icon="icon: logo; ">
 
                     </a>
 
@@ -14,9 +18,9 @@
                             <a href="/">Home</a>
                         </li>
                         @auth
-                        <li class="{{ request()->routeIs('dashboard') ? 'uk-active' : '' }}">
-                            <a href="{{ route('dashboard') }}">Dashboard</a>
-                        </li>
+                            <li class="{{ request()->routeIs('dashboard.index') ? 'uk-active' : '' }}">
+                                <a href="{{ route('dashboard.index') }}">Dashboard</a>
+                            </li>
                         @endauth
                         <li>
                             <a href="#">How it Works</a>
@@ -32,69 +36,81 @@
 
                 <div class="uk-navbar-right">
                     <div class="uk-navbar-item uk-hidden@m">
-                        <a class="uk-navbar-toggle " href="#mobile-nav" uk-toggle="animation: true"
-                            aria-expanded="false">
-                            <div uk-navbar-toggle-icon="" class="uk-icon uk-navbar-toggle-icon">
+                        <a class="uk-navbar-toggle "
+                           href="#mobile-nav"
+                           uk-toggle="animation: true"
+                           aria-expanded="false">
+                            <div uk-navbar-toggle-icon=""
+                                 class="uk-icon uk-navbar-toggle-icon">
                             </div>
                         </a>
                     </div>
                     <ul class="uk-navbar-nav uk-visible@m">
                         @guest
-                        <li class="{{ request()->routeIs('login') ? 'uk-active' : '' }}">
-                            <a href="{{ route('login') }}">Login</a>
-                        </li>
+                            <li class="{{ request()->routeIs('login') ? 'uk-active' : '' }}">
+                                <a href="{{ route('login') }}">Login</a>
+                            </li>
                         @else
-                        <li>
-                            <a href="#">
-                                <img src="{{ asset('storage/assets/img/dashboard-icon.svg') }}"
-                                    alt="Homer Simpson Avatar" width="50" height="50" loading="lazy"
-                                    class="uk-border-circle">
-                            </a>
-                            <div class="uk-navbar-dropdown uk-width-medium">
-                                <ul class="uk-nav uk-navbar-dropdown-nav">
-                                    <li class="uk-nav-header">
-                                        Hi, {{ Auth::user()->name }}
-                                    </li>
+                            <li>
+                                <a href="#">
+                                    <img src="{{ asset('storage/assets/img/dashboard-icon.svg') }}"
+                                         alt="Homer Simpson Avatar"
+                                         width="50"
+                                         height="50"
+                                         loading="lazy"
+                                         class="uk-border-circle">
+                                </a>
+                                <div class="uk-navbar-dropdown uk-width-medium">
+                                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                                        <li class="uk-nav-header">
+                                            Hi, {{ Auth::user()->name }}
+                                        </li>
 
-                                    <li class="uk-nav-divider"></li>
+                                        <li class="uk-nav-divider"></li>
 
-                                    <li>
-                                        <a href="/user/profile">
-                                            <span uk-icon="user" class="uk-margin-small-right"></span>
-                                            Profile
-                                        </a>
-                                    </li>
+                                        <li>
+                                            <a href="/user/profile">
+                                                <span uk-icon="icon: user;ratio: 1.2"
+                                                      class="uk-margin-small-right"></span>
+                                                Profile
+                                            </a>
+                                        </li>
 
-                                    <li>
-                                       <x-logout/>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
+                                        <li>
+                                            <x-logout />
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                     <div class="uk-navbar-item uk-visible@m">
-                        <a class="uk-button uk-button-primary uk-border-rounded" href="{{ route('listing.create') }}">Create a Listing</a>
+                        <a class="uk-button uk-button-primary uk-border-rounded"
+                           href="{{ route('listing.create') }}">Create a Listing</a>
                     </div>
                 </div>
             </nav>
         </div>
 
         <aside class="uk-position-relative uk-position-z-index">
-            <div class="uk-position-top" id="mobile-nav" hidden>
+            <div class="uk-position-top"
+                 id="mobile-nav"
+                 hidden>
                 <div class="uk-background-default uk-box-shadow-small uk-padding">
-                    <div class="uk-child-width-1-1 uk-grid uk-grid-stack" uk-grid>
+                    <div class="uk-child-width-1-1 uk-grid uk-grid-stack"
+                         uk-grid>
                         <div>
-                            <nav class="uk-panel" role="main-mobile-navigation">
+                            <nav class="uk-panel"
+                                 role="main-mobile-navigation">
                                 <ul class="uk-nav uk-nav-default uk-nav-divider uk-nav-parent-icon uk-nav-accordion"
                                     uk-nav="targets: > .js-accordion;">
                                     <li class="{{ request()->is('/') ? 'uk-active' : '' }}">
                                         <a href="/">Home</a>
                                     </li>
                                     @auth
-                                    <li class="{{ request()->routeIs('dashboard') ? 'uk-active' : '' }}">
-                                        <a href="{{ route('dashboard') }}">Dashboard</a>
-                                    </li>
+                                        <li class="{{ request()->routeIs('dashboard.index') ? 'uk-active' : '' }}">
+                                            <a href="{{ route('dashboard.index') }}">Dashboard</a>
+                                        </li>
                                     @endauth
                                     <li class="">
                                         <a href="#">About us</a>
@@ -106,60 +122,57 @@
                                         <a href="#">Contact</a>
                                     </li>
                                     @guest
-                                    <li>
-                                        <a href="{{ route('login') }}">Login</a>
-                                    </li>
+                                        <li>
+                                            <a href="{{ route('login') }}">Login</a>
+                                        </li>
                                     @else
-                                    <li class="uk-parent js-accordion">
-                                        <a href="#" class="uk-flex uk-flex-middle">
+                                        <li class="uk-parent js-accordion">
+                                            <a href="#"
+                                               class="uk-flex uk-flex-middle">
 
-                                            <img src="{{ asset('storage/assets/img/dashboard-icon.svg') }}"
-                                                alt="Homer Simpson Avatar" width="50" height="50" loading="lazy"
-                                                class="uk-border-circle">
-                                            <span>
-                                                Your settings
-                                            </span>
-                                        </a>
-                                        <ul class="uk-nav-sub">
-                                            <li class="uk-nav-header">
-                                                Hi, {{ Auth::user()->name }}
-                                            </li>
+                                                <img src="{{ asset('storage/assets/img/dashboard-icon.svg') }}"
+                                                     alt="Homer Simpson Avatar"
+                                                     width="50"
+                                                     height="50"
+                                                     loading="lazy"
+                                                     class="uk-border-circle">
+                                                <span>
+                                                    Your settings
+                                                </span>
+                                            </a>
+                                            <ul class="uk-nav-sub">
+                                                <li class="uk-nav-header">
+                                                    Hi, {{ Auth::user()->name }}
+                                                </li>
 
-                                            <li class="uk-nav-divider"></li>
+                                                <li class="uk-nav-divider"></li>
 
-                                            <li>
-                                                <a href="/user/profile">
-                                                    <span uk-icon="user" class="uk-margin-small-right"></span>
-                                                    Profile
-                                                </a>
-                                            </li>
+                                                <li>
+                                                    <a href="/user/profile">
+                                                        <span uk-icon="user"
+                                                              class="uk-margin-small-right"></span>
+                                                        Profile
+                                                    </a>
+                                                </li>
 
-                                            <li>
-                                                <a href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                                    <span uk-icon="sign-out" class="uk-margin-small-right"></span>
-                                                    Logout
-                                                </a>
+                                                <li>
+                                                    <x-logout />
+                                                </li>
 
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                    uk-hidden>
-                                                    @csrf
-                                                </form>
-                                            </li>
-
-                                        </ul>
-                                    </li>
+                                            </ul>
+                                        </li>
                                     @endguest
                                 </ul>
 
                             </nav>
                         </div>
                         <div class="uk-grid-margin uk-first-column">
-                            <footer class="uk-panel" aria-label="mobile-navigation-footer">
+                            <footer class="uk-panel"
+                                    aria-label="mobile-navigation-footer">
                                 <div class="uk-panel">
                                     <div>
                                         <a class="uk-button uk-button-primary uk-button-large uk-border-rounded uk-width-1-1"
-                                            href="{{ route('listing.create') }}">Create a Listing</a>
+                                           href="{{ route('listing.create') }}">Create a Listing</a>
                                     </div>
                                     <hr>
                                     <p class="uk-text-meta uk-text-center">©
@@ -176,20 +189,23 @@
                                     </p>
 
                                     <div class="uk-child-width-auto uk-grid-small uk-flex uk-flex-center uk-grid"
-                                        uk-grid>
+                                         uk-grid>
                                         <div>
-                                            <a class="uk-icon-link uk-icon" href="#"
-                                                uk-icon="icon: facebook;">
+                                            <a class="uk-icon-link uk-icon"
+                                               href="#"
+                                               uk-icon="icon: facebook;">
                                             </a>
                                         </div>
                                         <div>
-                                            <a class="uk-icon-link uk-icon" href="#"
-                                                uk-icon="icon: twitter;">
+                                            <a class="uk-icon-link uk-icon"
+                                               href="#"
+                                               uk-icon="icon: twitter;">
                                             </a>
                                         </div>
                                         <div>
-                                            <a class="uk-icon-link uk-icon" href="https://github.com/bonge-ian"
-                                                uk-icon="icon: github;">
+                                            <a class="uk-icon-link uk-icon"
+                                               href="https://github.com/bonge-ian"
+                                               uk-icon="icon: github;">
                                             </a>
                                         </div>
 
