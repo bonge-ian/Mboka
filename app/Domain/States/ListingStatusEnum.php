b6e27dfd94db61e383625fd4851cc92a53d4bde3
Bonge <nkaranjah@gmail.com>
2022-05-17 16:37:56 +0300

    Added color() to match enum instance to uikit css color clases

--- app/Domain/States/ListingStatusEnum.php
+++ app/Domain/States/ListingStatusEnum.php
@@ -6,11 +6,20 @@ namespace App\Domain\States;
 
 use App\Domain\States\Concerns\GetValues;
 
-enum ListingStatusEnum : string
+enum ListingStatusEnum: string
 {
     use GetValues;
 
     case ACTIVE = 'active';
     case PENDING = 'pending';
     case CLOSED = 'closed';
+
+    public function color(): string
+    {
+        return match ($this) {
+            ListingStatusEnum::ACTIVE => 'success',
+            ListingStatusEnum::PENDING => 'warning',
+            ListingStatusEnum::CLOSED => 'muted',
+        };
+    }
 }
