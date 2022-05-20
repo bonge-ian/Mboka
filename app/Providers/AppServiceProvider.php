d4b1f317d0b5f889dcff0545a23c21d7382d5ff4
Bonge <nkaranjah@gmail.com>
2022-05-20 12:56:50 +0300

    Added a can-edit-listing permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Listing;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define(
+            ability: 'can-edit-listing',
+            callback: fn (User $user, Listing $listing) => $user->id === $listing->user_id
+        );
     }
 }
