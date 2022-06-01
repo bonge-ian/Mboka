912021e47dee767e3998a7721965ef44d4a321dc
Bonge <nkaranjah@gmail.com>
2022-06-01 11:41:33 +0300

    Refractor: Removed properties and initialization code to trait

--- app/Http/Livewire/Concerns/AddorEditListingHelpers.php
+++ app/Http/Livewire/Concerns/AddorEditListingHelpers.php
@@ -4,13 +4,56 @@ declare(strict_types=1);
 
 namespace App\Http\Livewire\Concerns;
 
+use App\Models\Listing;
 use App\Models\Category;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Collection;
 use App\Domain\States\ListingTypeEnum;
+use App\Http\Livewire\Dashboard\EditListing;
 use App\Domain\States\EmployeeAvailabilityEnum;
 
 trait AddOrEditListingHelpers
 {
+    public string $perks = '';
+
+    public string $tags = '';
+
+    public string $title = '';
+
+    public int $category_id = 0;
+
+    public string $overview = '';
+
+    public string $location = '';
+
+    public string $apply_url = '';
+
+    public ?int $on_site_days = null;
+
+    public string $experience = '';
+
+    public string $listing_type = '';
+
+    public string $employee_availability = '';
+
+    public function initialize(array|Collection $data)
+    {
+        $this->fill([
+            'perks' => data_get($data, 'perks'),
+            'title' => data_get($data, 'title'),
+            'location' => data_get($data, 'location'),
+            'overview' => data_get($data, 'overview'),
+            'apply_url' => data_get($data, 'apply_url'),
+            'experience' => data_get($data, 'experience'),
+            'category_id' => data_get($data, 'category_id'),
+            'on_site_days' => data_get($data, 'on_site_days'),
+            'listing_type' => data_get($data, 'listing_type'),
+            'employee_availability' => data_get($data, 'employee_availability'),
+        ]);
+    }
+
     public function getCategoriesProperty()
     {
         return Category::pluck('name', 'id');
@@ -40,6 +83,17 @@ trait AddOrEditListingHelpers
         $this->showOnsiteDays = $this->processShowOnsiteDays($value);
     }
 
+
+    public function unpackTags(?string $tags = null): array
+    {
+        $tags ??= $this->tags;
+
+        return explode(
+            separator: ',',
+            string: Str::replace(', ', ',', $tags)
+        );
+    }
+
     protected function processShowOnsiteDays(string $value): bool
     {
         return match (EmployeeAvailabilityEnum::tryFrom($value)) {
@@ -50,4 +104,43 @@ trait AddOrEditListingHelpers
         };
     }
 
+
+    protected function rules(): array
+    {
+        $rules = Arr::except(Listing::rules(), [
+            'highlight_color',
+            'is_highlighted',
+            'is_pinned',
+            'show_logo',
+        ]);
+
+        $rules['on_site_days'] = [
+            'nullable',
+            Rule::requiredIf(
+                fn () => $this->processShowOnsiteDays($this->employee_availability)
+            ),
+            'numeric',
+            'min:1',
+            'max:5'
+        ];
+
+        $additionalRules = [
+            'tags' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    $splitTags = explode(
+                        ',',
+                        Str::replace(', ', ',', $value)
+                    );
+
+                    if (count($splitTags) > 5) {
+                        $fail("Only a maximum of 5 tags is required.");
+                    }
+                }
+            ]
+        ];
+
+        return array_merge($rules, $additionalRules);
+    }
 }
--- app/Http/Livewire/ListingDetailsForm.php
+++ app/Http/Livewire/ListingDetailsForm.php
@@ -22,43 +22,10 @@ class ListingDetailsForm extends Component
 
     public int $step = 1;
 
-    public string $perks = '';
-
-    public string $tags = '';
-
-    public string $title = '';
-
-    public int $category_id = 0;
-
-    public string $overview = '';
-
-    public string $location = '';
-
-    public string $apply_url = '';
-
-    public ?int $on_site_days = null;
-
-    public string $experience = '';
-
-    public string $listing_type = '';
-
-    public string $employee_availability = '';
-
     public function mount()
     {
         if (!empty($this->state->get('listing'))) {
-            $this->fill([
-                'perks' => data_get($this->state, 'listing.perks'),
-                'title' => data_get($this->state, 'listing.title'),
-                'location' => data_get($this->state, 'listing.location'),
-                'overview' => data_get($this->state, 'listing.overview'),
-                'apply_url' => data_get($this->state, 'listing.apply_url'),
-                'experience' => data_get($this->state, 'listing.experience'),
-                'category_id' => data_get($this->state, 'listing.category_id'),
-                'on_site_days' => data_get($this->state, 'listing.on_site_days'),
-                'listing_type' => data_get($this->state, 'listing.listing_type'),
-                'employee_availability' => data_get($this->state, 'listing.employee_availability'),
-            ]);
+            $this->initialize((array) $this->state->get('listing'));
         }
 
         if (!empty($this->state->get('tags'))) {
@@ -84,55 +51,9 @@ class ListingDetailsForm extends Component
         $this->resetErrorBag();
     }
 
-    public function unpackTags(): array
-    {
-        return explode(
-            separator: ',',
-            string: Str::replace(', ', ',', $this->tags)
-        );
-    }
-
     public function render(): View
     {
         return view('livewire.listing-details-form');
     }
-
-    protected function rules(): array
-    {
-        $rules = Arr::except(Listing::rules(), [
-            'highlight_color',
-            'is_highlighted',
-            'is_pinned',
-            'show_logo',
-        ]);
-
-        $rules['on_site_days'] = [
-            'nullable',
-            Rule::requiredIf(
-                fn () => $this->processShowOnsiteDays($this->employee_availability)
-            ),
-            'numeric',
-            'min:1',
-            'max:5'
-        ];
-
-        $additionalRules = [
-            'tags' => [
-                'required',
-                'string',
-                function ($attribute, $value, $fail) {
-                    $splitTags = explode(
-                        ',',
-                        Str::replace(', ', ',', $value)
-                    );
-
-                    if (count($splitTags) > 5) {
-                        $fail("Only a maximum of 5 tags is required.");
-                    }
-                }
-            ]
-        ];
-
-        return array_merge($rules, $additionalRules);
-    }
+    
 }
