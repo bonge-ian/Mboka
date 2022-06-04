9f8067c12f6549339aa1ef5f15d42ecf0f1b83e1
Bonge <nkaranjah@gmail.com>
2022-06-04 13:33:33 +0300

    Changed amount property from float  to Money instance

--- app/Domain/ValueObjects/Factories/TransactionResponseFactory.php
+++ app/Domain/ValueObjects/Factories/TransactionResponseFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\ValueObjects\Factories;
 
 use Carbon\Carbon;
+use App\Domain\Helpers\Money;
 use App\Domain\ValueObjects\Flutterwave\TransactionResponse;
 use App\Domain\ValueObjects\Factories\Contracts\ValueObjectFactory;
 
@@ -15,7 +16,7 @@ class TransactionResponseFactory implements ValueObjectFactory
         return new TransactionResponse(
             id: $attributes['id'],
             transactionCode: $attributes['tx_ref'],
-            amount: floatval($attributes['amount']),
+            amount: new Money($attributes['amount']),
             paymentType: strval($attributes['payment_type']),
             created_at: Carbon::parse($attributes['created_at'])->setTimezone(config('app.timezone')),
             status: strval($attributes['status']),
--- app/Domain/ValueObjects/Flutterwave/TransactionResponse.php
+++ app/Domain/ValueObjects/Flutterwave/TransactionResponse.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\ValueObjects\Flutterwave;
 
 use Carbon\Carbon;
+use App\Domain\Helpers\Money;
 use App\Domain\ValueObjects\Contracts\ValueObject;
 
 class TransactionResponse implements ValueObject
@@ -12,7 +13,7 @@ class TransactionResponse implements ValueObject
     public function __construct(
         public readonly int $id,
         public readonly string $transactionCode,
-        public readonly float $amount,
+        public readonly Money $amount,
         public readonly string $status,
         public readonly string $paymentType,
         public readonly Carbon $created_at,
